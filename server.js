c5725dcc70d15a25ac7504d7c6183c1033114ff0
TGOLD <thomaswgold90@gmail.com>
2022-08-24 18:51:03 -0600

    server.js start and file structure

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const api = require("./routes/index");
+
+const PORT = process.env.pot || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+console.log("Hi");
